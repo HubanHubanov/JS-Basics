4fad110481871eb2637f380e7031f28f237dc03c
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-08 16:30:38 +0100

    test1

--- 01. First Steps in Coding Lab/01.Hello Softuni.js
+++ 01. First Steps in Coding Lab/01.Hello Softuni.js
@@ -1,13 +1,4 @@
 function hello() {
     console.log("Hello SoftUni");
-    console.log('test123');
-    console.log('test123');
-    console.log('test123');
-    console.log('test123');
-    console.log('test123');
-    console.log('test123');
-
-
-
 }
 hello();
