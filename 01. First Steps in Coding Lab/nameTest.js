a6d58e463550129df3eaafe41c4029d366cc5716
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-09 19:09:57 +0100

    te

---
+++ 01. First Steps in Coding Lab/nameTest.js
@@ -0,0 +1,8 @@
+function test1(input) {
+let name = input[1];
+
+    console.log(name);
+
+}
+
+test1(["Ben", "John", "Petur"]);
---
+++ 01. First Steps in Coding Lab/nameTest3.js
@@ -0,0 +1,10 @@
+function test1(input) {
+let name = input[1];
+let age = Number(input[3]);
+
+    console.log(name);
+    console.log(age);
+
+}
+
+test1(["Ben", "John", "Petur", 25, 30, 26]);
