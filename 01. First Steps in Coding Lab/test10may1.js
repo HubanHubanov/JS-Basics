d55a20596c5410935fca47c682ea81353246142c
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-10 14:10:27 +0100

    add 05.greetingByName

---
+++ 01. First Steps in Coding Lab/test10may1.js
@@ -0,0 +1,9 @@
+function greetingByName(input) {
+    let name = input[0];
+    let age = Number(input[1]);
+
+console.log("Hello, " + name + "!");
+
+}
+
+greetingByName(["Huban"]);
--- 01. First Steps in Coding Lab/testarr.js
+++ 01. First Steps in Coding Lab/testarr.js
@@ -1,6 +1,7 @@
 function solve() {
     let arr =[1,2,3,4,5];
-    
+
+
     console.log(arr.length - 2);
 
 }
