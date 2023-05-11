70c76c8416cdb495ce42c4f087493e1b0927ba93
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-11 18:08:13 +0100

    add projectCreation1

---
+++ 01. First Steps in Coding Lab/projectCreation1.js
@@ -0,0 +1,13 @@
+function projectsCreation(input) {
+    let name = input[0];
+    let projects = Number(input[1]);
+    let hours = projects * 3;
+    let name1 = input[2];
+    let projects1 = Number(input[3]);
+    let hours1 = projects1 * 3;
+
+    console.log(`The architect ${name} will need ${hours} hours to complete ${projects} project/s while the architect ${name1} will need ${hours1} hours to complete ${projects1} project/s`);
+
+}
+
+projectsCreation(["George", "9", "John","10"]);
