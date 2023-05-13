1469e4ab6d506c74e8cf9977bb7e6f54132405ed
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-13 11:19:58 +0100

    add foodDelivery

---
+++ 01. First Steps in Coding Lab/foodDelivery.js
@@ -0,0 +1,21 @@
+function foodDelivey(input) {
+    let fish = Number(input[0]);
+    let chicken =Number(input[1]);
+    let veggie = Number(input[2]);
+
+    let fishCost = fish * 10.35;
+    let chickenCost = chicken * 12.40;
+    let veggieCost = veggie * 8.15;
+
+    let mainFood = fishCost + chickenCost + veggieCost;
+    let dessert = 0.2 * mainFood;
+
+    let food = dessert + mainFood;
+    let cost = food + 2.5;
+
+    console.log(cost);
+        
+
+}
+
+foodDelivey(["2", "4", "3"]);
