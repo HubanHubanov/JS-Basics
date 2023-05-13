1e6de7561ada72432fdfadfd5cfe1db03d75d35a
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-13 17:14:21 +0100

    add equipmentTable

---
+++ 01. First Steps in Coding Lab/equipmentTable.js
@@ -0,0 +1,14 @@
+function basketballEquipmnet(input) {
+    let tax = Number(input[0]);
+    let sneakers = tax - 0.4 * tax;
+    let clothes = sneakers - 0.2 * sneakers;
+    let ball = 0.25 * clothes;
+    let accesories = 0.2 * ball;
+    let expenses = tax + sneakers + clothes + ball + accesories;
+  
+    
+    console.table([tax, sneakers, clothes, ball, accesories, expenses]);
+
+  }
+  
+basketballEquipmnet(["550"]);
