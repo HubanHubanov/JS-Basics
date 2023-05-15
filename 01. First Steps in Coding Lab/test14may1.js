0e7d4b53b8c887c5af401bed64741b788f348e13
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-15 17:08:45 +0100

    add trapezoidArea

---
+++ 01. First Steps in Coding Lab/test14may1.js
@@ -0,0 +1,10 @@
+function petShop(input) {
+    let dogFood = input[0];
+    let catFood = input[1];
+    sum = dogFood * 2.50 + catFood * 4.00;
+
+    console.log(`The sum is ${sum} lv.`);
+
+}
+
+petShop(["5", "4"]);
