8982929a7778dcf43e8137ce81c2aa59711e1b16
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-16 16:33:52 +0100

    add 06.Repainting

--- 02. First Steps in Coding Exercise/07.Repainting.js
+++ 02. First Steps in Coding Exercise/07.Repainting.js
@@ -1,30 +1,24 @@
 function repainting(input) {
-    let nylon = Number(input[0]);
-    let paint = Number(input[1]);
-    let paintThinner = Number(input[2]);
-    let hoursNeeded = Number(input[3]);
-    
-    let nylonCost = nylon * 1.50;
-    let paintCost = paint * 14.50;
-    let paintThinnerCost = paintThinner * 5.00;
-    
-    let paintCostTotal = paintCost + 0.1 * paintCost;
-    let nylonCostTotal = nylonCost + 2 * 1.50;
-    
-    let materials = paintCostTotal + nylonCostTotal + paintThinnerCost + 0.40;
-    
-    let labourCostPerHour = 0.30 * materials;
-    let labourCost = labourCostPerHour * hoursNeeded;
-    
-    let finalCost = materials + labourCost;
+  let nylon = Number(input[0]);
+  let paint = Number(input[1]);
+  let paintThinner = Number(input[2]);
+  let hoursNeeded = Number(input[3]);
 
-    console.log(finalCost);
+  let nylonCost = nylon * 1.5;
+  let paintCost = paint * 14.5;
+  let paintThinnerCost = paintThinner * 5.0;
 
+  let paintCostTotal = paintCost + 0.1 * paintCost;
+  let nylonCostTotal = nylonCost + 2 * 1.5;
+
+  let materials = paintCostTotal + nylonCostTotal + paintThinnerCost + 0.4;
+
+  let labourCostPerHour = 0.3 * materials;
+  let labourCost = labourCostPerHour * hoursNeeded;
+
+  let finalCost = materials + labourCost;
+
+  console.log(finalCost);
 }
 
 repainting(["10", "11", "4", "8"]);
-
-
-
-
-
