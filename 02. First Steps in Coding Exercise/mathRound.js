63ce710e84ed196e0672fe26b199cd92a5c07fe9
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-16 23:14:00 +0100

    add MathRoundCeil.Floor

---
+++ 02. First Steps in Coding Exercise/mathRound.js
@@ -0,0 +1,20 @@
+function test() {
+  let a = 2.01;
+  let b = 2.99;
+  let c = 3.49;
+  let d = 3.50;
+
+  let a1 = Math.ceil(a);
+  let b1 = Math.floor(b);
+  let c1 = Math.round(c);
+  let d1 = Math.round(d);
+  
+  console.log(a1);
+  console.log(b1);
+  console.log(c1);
+  console.log(d1);
+
+
+}
+
+test();
