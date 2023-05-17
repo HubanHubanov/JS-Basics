46ee6ed80316936f458ee4785c8127b6fecc5e02
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-17 18:00:30 +0100

    add test17may

---
+++ 02. First Steps in Coding Exercise/agadgadga.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    let name = input[0]
+  
+    console.log(`Hello, ${name} !`);
+    console.log(`Good bye, ${name} !`);
+}
+
+solve(["Peter"]);
+solve(["John"]);
+solve(["Adam"]);
