64c6132b985ae23dc8b1bcbbf91862e46b72d5d0
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-25 21:54:21 +0100

    add testmay25

---
+++ 04.Conditional Statements - Exercise/testmay25.js
@@ -0,0 +1,22 @@
+function solve(input) {
+    let day = Number(input[0]);
+
+    switch (day) {
+        case 1: console.log("Tom"); break;
+        case 2: console.log("Jim"); break;
+        case 3: console.log("Tania"); break; 
+        case 4: console.log("Svetla"); break;
+        case 5: console.log("Mira"); break;
+        case 6: console.log("Ilia"); break;
+        case 7: console.log("Todory"); break;
+        case 8: console.log("Ben"); break;
+        case 9: console.log("Vili"); break;
+        case 10: console.log("Huban"); break;
+        default: console.log("Elmo"); break;
+
+    }
+}
+
+solve("8")
+
+
