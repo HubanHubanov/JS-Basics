a2481c4b9a37676b777f00fcec4061328e3194eb
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-26 12:18:48 +0100

    add test 26may

---
+++ 04.Conditional Statements - Exercise/test26may.js
@@ -0,0 +1,22 @@
+function solve(input) {
+  let age = Number(input[0]);
+  let gender = input[1];
+
+  if (age >= 16) {
+    if (gender === "f") {
+      console.log("Ms.");
+    } else {
+      console.log("Mr.");
+    }
+  } else {
+    if (gender === "f") {
+      console.log("Miss");
+    } else {
+      console.log("Master");
+    }
+  }
+}
+
+solve(["1.5", "m"]);
+
+
