5d11f4bf76c313aa11e4db1ffb4dd8f47b585c7b
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-28 20:41:32 +0100

    add 10.InvalidNumber

---
+++ 05.Conditional Statements Advanced - Lab/10.InvalidNumber.js
@@ -0,0 +1,15 @@
+function invalidNumber(input) {
+  let num = Number(input[0]);
+
+  if (num < 100) {
+    if (num === 0) {
+    } else {
+      console.log("invalid");
+    }
+  } else if (num > 200) {
+    console.log("invalid");
+  } else {
+  }
+}
+
+invalidNumber(["200"]);
