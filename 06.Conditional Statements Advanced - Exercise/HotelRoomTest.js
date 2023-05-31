4a5184fc56839a9db79cdbbbc6792dc6b41233ae
Huban Hubanov <huban.hubanov@gmail.com>
2023-05-31 21:18:16 +0100

    add HotelRoomTest

---
+++ 06.Conditional Statements Advanced - Exercise/HotelRoomTest.js
@@ -0,0 +1,37 @@
+function hotelRoom(input) {
+    let month = input[0];
+    let nights = Number(input[1]);
+    let studio = 0;
+    let apartment = 0;
+  
+    if (month === "May" || month === "October") {
+      studio = 50;
+      apartment = 65;
+      if (nights > 7 && nights <= 14){
+        studio *= 0.95;
+      } else if (nights > 14) {
+        studio *= 0.7;
+        apartment *= 0.9;
+      }
+    } else if (month === "June" || month === "September") {
+      studio = 75.2;
+      apartment = 68.7;
+      if (nights > 14) {
+        studio *= 0.8;
+      apartment *= 0.9;
+      }
+
+
+    } else if (month === "July" || month === "August") {
+      studio = 76;
+      apartment = 77;
+      if (nights > 14){
+        apartment *= 0.9;
+      }
+    }
+  
+    
+    console.log(`Apartment: ${(nights * apartment).toFixed(2)} lv.`);
+    console.log(`Studio: ${(nights * studio).toFixed(2)} lv.`);
+  }
+  hotelRoom(["August", "20"]);
