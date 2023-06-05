374187436a1db68954cd2835ae90d60a393f157f
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-05 13:55:30 +0100

    add 02.MultiplicationTable

---
+++ 08. For Loop - Exercise/02.MultiplicationTable.js
@@ -0,0 +1,9 @@
+function multipclicationTable(input) {
+  let num = Number(input[0]);
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${i} * ${num} = ${i * num}`);
+  }
+}
+
+multipclicationTable(["5"]);
