fb284a8837e808170b490d899273adf5725edf58
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-05 10:24:28 +0100

    add 07.SumOfDigits

---
+++ 07.For Loop - Lab/07.SumOfNumbers.js
@@ -0,0 +1,13 @@
+function sumOfNubers(input) {
+  let numberAsText = input[0].toString();
+  let sum = 0;
+
+  for (let i = 0; i < numberAsText.length; i++) {
+    let num = Number(numberAsText[i]);
+    sum += num;
+  }
+
+  console.log(`The sum of the digits is:${sum}`);
+}
+
+sumOfNubers(["1234"]);
