693439e597dbd0d3a1cedd7129d0b381f609d879
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-05 18:28:52 +0100

    add 03.Histogram

--- 08. For Loop - Exercise/02.MultiplicationTable.js
+++ 08. For Loop - Exercise/02.MultiplicationTable.js
@@ -1,8 +1,10 @@
 function multipclicationTable(input) {
   let num = Number(input[0]);
+  let result = 0;
 
   for (let i = 1; i <= 10; i++) {
-    console.log(`${i} * ${num} = ${i * num}`);
+    result = i * num;
+    console.log(`${i} * ${num} = ${result}`);
   }
 }
 
