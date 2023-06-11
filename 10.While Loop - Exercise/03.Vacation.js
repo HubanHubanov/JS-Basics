5407474f4497d57e5eebcdf6c8fc748e45a23b80
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-11 18:53:12 +0100

    add 03.Vacation

---
+++ 10.While Loop - Exercise/03.Vacation.js
@@ -0,0 +1,62 @@
+function vacation(input) {
+  let index = 0;
+  let moneyNeeded = Number(input[index]);
+  index++;
+  let moneyAvailabe = Number(input[index]);
+  index++;
+
+  let command = input[index];
+  index++;
+
+  let money = Number(input[index]);
+  index++;
+
+  consecutiveDaysSpending = 0;
+  let daysTotal = 0;
+
+  let savedSum = moneyAvailabe;
+
+  let saveTheMoney = false;
+
+  while (savedSum <= moneyNeeded) {
+    if (command === "spend") {
+      savedSum -= money;
+      if (savedSum < money) {
+        savedSum = 0;
+      }
+      consecutiveDaysSpending++;
+      daysTotal++;
+    } else if (command === "save") {
+      savedSum += money;
+      consecutiveDaysSpending = 0;
+      daysTotal++;
+
+      if (savedSum >= moneyNeeded) {
+        break;
+      }
+    }
+
+    if (consecutiveDaysSpending === 5) {
+      saveTheMoney = true;
+      console.log("You can't save the money.");
+      console.log(daysTotal);
+      break;
+    }
+
+    command = input[index];
+    index++;
+
+    money = Number(input[index]);
+    index++;
+  }
+  if (!saveTheMoney) {
+    console.log(`You saved the money for ${daysTotal} days.`);
+  }
+}
+
+vacation(["2000",
+"1000",
+"spend",
+"1200",
+"save",
+"2000"])
