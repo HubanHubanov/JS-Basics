c6588fdf3c3cf14d97b43f41c306ab9c89938c56
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-17 17:34:59 +0100

    add  EvenPowersTest

---
+++ 08. For Loop - Exercise/EvenPowersOf2.js
@@ -0,0 +1,17 @@
+function solve(input) {
+    let n = Number(input[0]);
+    
+    for (let i = 0; i <= n; i++) {
+        if (i % 2 === 0) {
+            console.log(Math.pow(2, i));
+        }
+        
+        
+    }
+    
+
+
+
+
+}
+solve(["3"])
