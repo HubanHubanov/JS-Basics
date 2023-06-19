ec40bd3b676f60dff1b3d9d053b6a364a012e4fd
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-20 00:03:58 +0100

    add 03.SumPrimeNonPrime

---
+++ 12.Nested Loops - Exercise/03.SumPrimeNonPrime.js
@@ -0,0 +1,39 @@
+function test(input) {
+  let index = 0;
+  let currentInput = input[index];
+  let primeSum = 0;
+  let nonPrimeSum = 0;
+  let isNonPrime = false;
+
+  while (currentInput !== "stop") {
+    let number = Number(currentInput);
+
+    if (number < 0) {
+      console.log("Number is negative.");
+      index++;
+      currentInput = input[index];
+      continue;
+    }
+
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        isNonPrime = true;
+        break;
+      }
+    }
+
+    index++;
+    currentInput = input[index];
+
+    if (isNonPrime) {
+      nonPrimeSum += number;
+      isNonPrime = false;
+    } else {
+      primeSum += number;
+    }
+  }
+  console.log(`Sum of all prime numbers is: ${primeSum}`);
+  console.log(`Sum of all non prime numbers is: ${nonPrimeSum}`);
+}
+
+test(["0", "-9", "0", "stop"]);
