79fa9469af7706db53f0c54fe4e8a1c7e762e0dd
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-21 01:28:42 +0100

    add 02.EqualSumsEvenOddPosition

--- 12.Nested Loops - Exercise/02.EqualSumsEvenOddPosition.js
+++ 12.Nested Loops - Exercise/02.EqualSumsEvenOddPosition.js
@@ -1,27 +1,33 @@
 function equalSumsEvenOddPosition(input) {
-  let start = Number(input[0]);
-  let end = Number(input[1]);
+  let firstNumber = Number(input[0]);
+  let lastNumber = Number(input[1]);
 
-  let buff = "";
+  let evenSum = 0;
+  let oddSum = 0;
 
-  for (let i = start; i <= end; i++) {
-    let currentNum = "" + i;
-    let sumOdd = 0;
-    let sumEven = 0;
-    for (let j = 0; j < currentNum.length; j++) {
-      let currentDigit = Number(currentNum.charAt(j));
+  let printResult = "";
+
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    evenSum = 0;
+    oddSum = 0;
+    let buff = "";
+    buff += "" + i;
+    for (j = 0; j < buff.length; j++) {
+      let ch = Number(buff[j]);
+
       if (j % 2 === 0) {
-        sumEven += currentDigit;
+        oddSum += ch;
       } else {
-        sumOdd += currentDigit;
+        evenSum += ch;
       }
-      if (sumEven === sumOdd) {
-        buff += `${i} `;
-      }
+    }
+
+    if (evenSum === oddSum) {
+      printResult += "" + buff + " ";
     }
   }
 
-  console.log(buff);
+  console.log(printResult);
 }
 
-equalSumsEvenOddPosition(["100000", "100050"]);
+equalSumsEvenOddPosition(["299900", "300000"]);
