2a7dde4c0a604ae425cb00d8b9130e0e4e9d499c
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-21 01:33:21 +0100

    update

--- 12.Nested Loops - Exercise/02.EqualSumsEvenOddPosition.js
+++ 12.Nested Loops - Exercise/02.EqualSumsEvenOddPosition.js
@@ -2,14 +2,11 @@ function equalSumsEvenOddPosition(input) {
   let firstNumber = Number(input[0]);
   let lastNumber = Number(input[1]);
 
-  let evenSum = 0;
-  let oddSum = 0;
-
   let printResult = "";
 
   for (let i = firstNumber; i <= lastNumber; i++) {
-    evenSum = 0;
-    oddSum = 0;
+    let evenSum = 0;
+    let oddSum = 0;
     let buff = "";
     buff += "" + i;
     for (j = 0; j < buff.length; j++) {
