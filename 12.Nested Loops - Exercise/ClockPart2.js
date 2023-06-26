49bf6e96d0087eaecc87950f2d2bbd76da9ceb87
Huban Hubanov <huban.hubanov@gmail.com>
2023-06-27 00:20:00 +0100

    add ClockPart2

---
+++ 12.Nested Loops - Exercise/ClockPart2.js
@@ -0,0 +1,14 @@
+function clockPart2() {
+    
+    for (let hours = 0; hours < 24; hours++) {
+        for (let mins = 0; mins < 60; mins++) {
+            for (let secs = 0; secs < 60; secs++) {
+                console.log(`${hours} : ${mins} : ${secs}`);
+            }
+        }
+    }
+
+
+
+}
+clockPart2();
