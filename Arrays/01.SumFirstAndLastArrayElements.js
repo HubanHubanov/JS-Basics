32fe785928390b9c30d555217c33de8d2d2ec7d0
Huban Hubanov <huban.hubanov@gmail.com>
2023-08-01 21:33:19 +0100

    add 01.SumFirstAndLastArrayElements

---
+++ Arrays/01.SumFirstAndLastArrayElements.js
@@ -0,0 +1,13 @@
+function solve(input) {
+
+   let firsNum = input[0]
+   let lastNum = input[input.length -1]
+
+
+   console.log(firsNum +lastNum);
+
+
+
+}
+
+solve([20, 30, 40])
---
+++ Arrays/arrays.js
@@ -0,0 +1,19 @@
+function solve() {
+
+    let arr = [1, 2, 3, 4]
+    // arr[4] = arr[0] + arr[3]
+    // arr[0] = arr[0] + arr[1] + arr[2]
+    
+  
+
+    console.log(arr.length);
+    console.log(arr);
+
+    //  console.table(arr)
+    l
+
+
+
+}
+
+solve()
