fac339c7ec01ce086daae07c354c83db266bb690
Huban Hubanov <huban.hubanov@gmail.com>
2023-08-04 11:14:57 +0100

    add 07.EqualArrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,20 +1,25 @@
-function arrays(arr) {
+function solve(num) {
 
- a = 11
- b = 10
+  let res = [] 
+  
+  while (num.length > 1) {
+    for (let i = 0; i < num.length - 1; i ++) {
+    // // let sum = 0;
+    // //       sum += num[i] + num[i+1];
+    //       res.push(sum)
+      res[i] = num[i] + num[i+1]  
+      // console.log(res[i]);
+           console.log(i);
+        }
+     num = res;
+     res = []
 
-//  if (a < b) {
-//   console.log("yes");
-//  } else {
-//   console.log("no");
-//  }
-let isTrue = false
-
-a < b ? isTrue = true : isTrue
-
-console.log(isTrue);
-
+  
+   
+      }
+// console.log(num.toString());
 }
 
-arrays(['abc', 'def', 'hig',
-'klm', 'nop'])
+ 
+
+solve([2,10,3] )
