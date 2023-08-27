2dd3b8563571f764fcdf3992078ba102811a102d
Huban Hubanov <huban.hubanov@gmail.com>
2023-08-27 11:49:31 +0100

    update

--- Arrays Advanced - Lab/05.ProcesOddNumbers2.js
+++ Arrays Advanced - Lab/05.ProcesOddNumbers2.js
@@ -1,11 +1,12 @@
 function processOddNumbers(arr) {
+  let res = arr
+    .filter((x, i) => i % 2 === 1)
+    .map((z) => z * 2)
+    .reverse()
+    .join(" ");
 
-    let res = arr.filter((x ,i) => (i % 2 === 1)).map(z => z *2).reverse().join(" ")
-
-    return res
-
+  return res;
 }
 
-  console.log(processOddNumbers([10, 15, 20, 25]));
-  console.log(processOddNumbers([3, 0, 10, 4, 7, 3]));
-  
+console.log(processOddNumbers([10, 15, 20, 25]));
+console.log(processOddNumbers([3, 0, 10, 4, 7, 3]));
