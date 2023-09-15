8ac837a2d90964d7050c76fabc3aac08057bae9b
Huban Hubanov <huban.hubanov@gmail.com>
2023-09-15 11:51:42 +0100

    update

--- Objects and Classes - Exercise/04.Movies2.js
+++ Objects and Classes - Exercise/04.Movies2.js
@@ -1,14 +1,15 @@
-function movies(input) {
-  let movies = [];
+function movies(data) {
+  let arr = [];
 
-  input.forEach((line) => {
+  data.forEach((line) => {
     if (line.includes("addMovie ")) {
       let name = line.split("addMovie ")[1];
-      movies.push({ name });
+
+      arr.push({ name });
     } else if (line.includes("directedBy")) {
       let [name, director] = line.split(" directedBy ");
 
-      let movie = movies.find((m) => m.name === name);
+      let movie = arr.find((x) => x.name === name);
 
       if (movie) {
         movie.director = director;
@@ -16,7 +17,7 @@ function movies(input) {
     } else if (line.includes(" onDate ")) {
       let [name, date] = line.split(" onDate ");
 
-      let movie = movies.find((m) => m.name === name);
+      let movie = arr.find((x) => x.name === name);
 
       if (movie) {
         movie.date = date;
@@ -24,7 +25,7 @@ function movies(input) {
     }
   });
 
-  movies.forEach((movie) => {
+  arr.forEach((movie) => {
     if (movie.name && movie.director && movie.date) {
       console.log(JSON.stringify(movie));
     }
@@ -41,3 +42,11 @@ movies([
   "Batman onDate 01.08.2018",
   "Fast and Furious directedBy Rob Cohen",
 ]);
+// movies([
+//   "addMovie The Avengers",
+//   "addMovie Superman",
+//   "The Avengers directedBy Anthony Russo",
+//   "The Avengers onDate 30.07.2010",
+//   "Captain America onDate 30.07.2010",
+//   "Captain America directedBy Joe Russo",
+// ]);
